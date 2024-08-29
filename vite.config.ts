27b7f1aac52ea2ba625f13e8d8056ebbf6bf4d6b
Duc Nguyen <duc.nguyen@ascenda.com>
2024-08-29 21:59:17 +0700

    remove eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
-import path from 'path'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
