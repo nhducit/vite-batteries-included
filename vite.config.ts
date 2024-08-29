efa5908f9e051195b0532b01756e6ba8a6265701
Duc Nguyen <duc.nguyen@ascenda.com>
2024-08-29 21:45:55 +0700

    Add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    eslint({
-      include: ['./src/**/*.ts', './src/**/*.tsx'],
-      exclude: [],
-    }),
+    // eslint({
+    //   include: ['./src/**/*.ts', './src/**/*.tsx'],
+    //   exclude: [],
+    // }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
