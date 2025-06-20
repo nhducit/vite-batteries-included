0f29ab6c32c958553fc2aa107d100274a545a758
nhducit <nhducit@users.noreply.github.com>
2025-06-20 15:53:02 +0700

    Refactor Tailwind CSS configuration and styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./src/**/*.{ts,tsx}'],
 }
