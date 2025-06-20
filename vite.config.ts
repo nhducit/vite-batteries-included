fb289888f881c6e7943de33cf1a184891343bc0a
nhducit <nhducit@users.noreply.github.com>
2025-06-20 16:09:46 +0700

    Update .nvmrc version to 24, remove components.json, and clean up vite.config.ts by removing commented linter configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // eslint({
-    //   include: ['./src/**/*.ts', './src/**/*.tsx'],
-    //   exclude: [],
-    // }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
