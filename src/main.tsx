11437079093fef1d5b6bdbfbcbb700b2b7f96379
nhducit <nhducit@users.noreply.github.com>
2025-06-20 15:54:03 +0700

    rename app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './application'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
